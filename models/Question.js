b319f83abc637721b473b059612fe3ff89e98163
Amy Jackson <aj81623@gmail.com>
2017-11-22 00:00:10 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -31,9 +31,12 @@ class Question{
   static Find(id){
     const sql = "SELECT * FROM questions WHERE id = ?"
     return new Promise(function(resolve){
-      const question = new Question(result.content)
-      question.id = result.id
-      resolve(question)
+      db.get(sql, [id], function(err, resolve){
+        question.result = result.content
+        const question = new Question(result.content)
+        question.id = result.id
+        resolve(question)
+      })
     })
   }
 }
